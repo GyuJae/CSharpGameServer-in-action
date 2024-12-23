ab3e105ce17962aab45d93d422e11025b90e2263
정규재 <wjdrbwo1206@naver.com>
2024-12-23 13:28:32 +0900

    feat: ThreadPool

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore;
 
 class Program
 {
+    static void MainThread(object state)
+    {
+        for(var i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Main Thread");
+        }
+        
+    }
+    
     static void Main(string[] args)
     {
+        ThreadPool.SetMinThreads(1, 1);
+        ThreadPool.SetMaxThreads(5, 5);
+
+        for (var i = 0; i < 4; i++)
+        {
+            ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                while (true)
+                {
+                }
+            });
+        }
         
+        ThreadPool.QueueUserWorkItem(MainThread);
+        
+        while(true) {}
     }
 }
 
