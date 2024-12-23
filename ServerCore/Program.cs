5029ba6b0591b36a608e1d8245b70fdc838047c6
정규재 <wjdrbwo1206@naver.com>
2024-12-23 13:54:12 +0900

    feat: Spatial Locality Cache

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,35 +6,35 @@ namespace ServerCore;
 
 class Program
 {
-    private static volatile bool _stop = false;
-
-    static void ThreadMain()
-    {
-        Console.WriteLine("Thread Start!");
-        
-        while (_stop == false)
-        {
-            // 누군가의 신호를 줄때까지 대기한다.
-        }
-        Console.WriteLine("Thread End!");
-    }
-
-    
     static void Main(string[] args)
     {
-        Task t = new Task(ThreadMain);
-        t.Start();
-        
-        Thread.Sleep(1000);
+        int [,] arr = new int[10000, 10000];
 
-        _stop = true;
-
-        Console.WriteLine("Stop 호출");
-        Console.WriteLine("종료 대기중");
+        {
+            long now = DateTime.Now.Ticks;
+            for (int y = 0; y < arr.GetLength(0); y++)
+            {
+                for (int x = 0; x < arr.GetLength(1); x++)
+                {
+                    arr[y, x] = 1;
+                }
+            }
+            long end = DateTime.Now.Ticks;
+            Console.WriteLine($"(y, x) 걸린 시간: {now - end}");
+        }
         
-        t.Wait();
-        
-        Console.WriteLine("종료 성공");
+        {
+            long now = DateTime.Now.Ticks;
+            for (int y = 0; y < arr.GetLength(0); y++)
+            {
+                for (int x = 0; x < arr.GetLength(1); x++)
+                {
+                    arr[x, y] = 1;
+                }
+            }
+            long end = DateTime.Now.Ticks;
+            Console.WriteLine($"(x, y) 걸린 시간: {now - end}");
+        }
     }
 }
 
