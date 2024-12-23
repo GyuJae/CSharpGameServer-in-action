b27bf6c4fd2f3eb850cd049492024b8c66d7e4f4
정규재 <wjdrbwo1206@naver.com>
2024-12-23 15:05:43 +0900

    Spin Lock impl

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,81 +4,74 @@ using System.Threading.Tasks;
 
 namespace ServerCore;
 
+
+class SpinLock
+{
+    volatile int _locked = 0;
+        
+    public void Acquire()
+    {
+        // // 동시에 들어 갈 수 있음;
+        // while (_locked)
+        // {
+        //     // 잠김이 풀리기를 기다린다.
+        // }
+        // _locked = true;
+        while (true)
+        {
+            // version. 1
+            // int original = Interlocked.Exchange(ref _locked, 1);
+            // if (original == 0) break;
+            
+            // CAS Compare-And-Swap
+            int expected = 0;
+            int desired = 1;
+            if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected) break;
+        }
+    }
+
+    public void Release()
+    {
+        _locked = 0;
+    }
+}
+
 class Program
 {
+    private static int _num = 0;
+    private static SpinLock _lock = new SpinLock();
 
-    class SessionManager
-    {
-        static object _lock = new object();
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-               UserManager.TestUser(); 
-            }
-        }
-
-       public static void TestSession()
-        {
-            lock (_lock)
-            {
-                
-            }
-        }
-    }
-
-    class UserManager
-    {
-        static object _lock = new object();
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-                
-            }
-        }
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-        
-    }
-    
-    private static int num = 0;
-    
     static void Thread1()
     {
-        for (int i = 0; i < 100000; i++)
+        for (int i = 0; i < 10000; i++)
         {
-            UserManager.Test();
+            _lock.Acquire();
+            _num++;
+            _lock.Release(); 
         }
     }
 
     static void Thread2()
     {
-        for (int i = 0; i < 100000; i++)
+        for (int i = 0; i < 10000; i++)
         {
-            SessionManager.Test();
+            _lock.Acquire();
+            _num--;
+            _lock.Release(); 
         }
     }
     
     static void Main(string[] args)
     {
-        Task t1 = new Task(Thread1);
-        Task t2 = new Task(Thread2);
+        var t1 = new Task(Thread1);
+        var t2 = new Task(Thread2);
         
         t1.Start();
         t2.Start();
         
         Task.WaitAll(t1, t2);
         
-        Console.WriteLine(1);
+        Console.WriteLine(_num);
     }
 }
 
