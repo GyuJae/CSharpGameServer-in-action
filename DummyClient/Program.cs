e81cd63277c3aa40e2dade31ca943d1c9ff5fe03
정규재 <wjdrbwo1206@naver.com>
2024-12-24 13:37:44 +0900

    C# Socket

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace DummyClient;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        // DNS
+        IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 7777);
+        
+        // Socket 설정
+        Socket socket = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        
+        try
+        {
+            socket.Connect(localEndPoint);
+            Console.WriteLine("Socket connected to {0}:{1}", localEndPoint.Address, socket.RemoteEndPoint?.ToString());
+
+
+            // Send
+            byte[] sendBuffer = Encoding.UTF8.GetBytes("Hello World");
+            int sendBytes = socket.Send(sendBuffer);
+
+            // Receive
+            byte[] receiveBuffer = new byte[1024];
+            int receiveBytes = socket.Receive(receiveBuffer);
+            string message = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
+            Console.WriteLine($"[Server]: {message}");
+
+            // 나간다
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
+
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,34 +1,55 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore;
 
-class Program
+internal abstract class Program
 {
-    private static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => $"My Name Is {Thread.CurrentThread.ManagedThreadId}");
-
-    static void WhoAmi()
-    {
-        bool repeat = ThreadName.IsValueCreated;
-        if (repeat)
-        {
-            Console.WriteLine(ThreadName.Value + "  (repeat)");
-        }
-        else
-        {
-            Console.WriteLine(ThreadName.Value);
-        }
-        
-        Thread.Sleep(1000);
-        Console.WriteLine(ThreadName.Value);
-    }
-    
     static void Main(string[] args)
     {
-        ThreadPool.SetMinThreads(1, 1);
-        ThreadPool.SetMaxThreads(3, 3);
+        // DNS
+        IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 7777);
         
-        Parallel.Invoke(WhoAmi, WhoAmi ,WhoAmi ,WhoAmi ,WhoAmi ,WhoAmi ,WhoAmi ,WhoAmi, WhoAmi , WhoAmi);
+        // Socket
+        Socket listenSocket = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        
+        try
+        {
+            // 교육
+            listenSocket.Bind(localEndPoint);
+        
+            // backlog : 최대 대기수
+            listenSocket.Listen(10);
+            
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+            
+                // Client 입장
+                Socket clientSocket = listenSocket.Accept();
+            
+                // Receive
+                byte[] recvBuff = new byte[1024];
+                int recvByte = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvByte);
+                Console.WriteLine($"[Client] {recvData}");
+            
+                // Send
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to the server!");
+                clientSocket.Send(sendBuff);
+            
+                // 쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
     }
 }
